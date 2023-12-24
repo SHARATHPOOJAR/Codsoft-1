271db5000ce36ededf26a01fd6936c8aa23c1031
SHARATHPOOJAR <154723331+SHARATHPOOJAR@users.noreply.github.com>
2023-12-24 19:36:01 +0530

    Add files via upload

---
+++ codesoft.py
@@ -0,0 +1,17 @@
+x=int(input("Enter the first number:"))
+y=int(input("Enter the second number:"))
+z=(input("Enter the operand [+,-,/,*]:"))
+if(z=="+"):
+    print("The sum is =",x+y)
+elif(z=="-"):
+    print("The difference is =",x-y)
+elif(z=="*"):
+    print("The product is =",x*y)
+elif(z=="/"):
+    print("The quotient is =",x/y) 
+else:
+    print("Invalid Entry")    
+
+         
+
+    
